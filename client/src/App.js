de1a7e1dd7afce710957732bf47d1ee32ed48431
jmbvdev <jmvb.dev@gmail.com>
2022-10-03 20:17:31 -0400

    modo oscuro y perros por pagina

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import LandingPage from './pages/LandingPage.jsx';
 import DogDetails from './pages/DogDetails.jsx';
 import CreateDog from './pages/CreateDog.jsx';
 import { useEffect } from 'react';
-import Config from './pages/Config.jsx';
+
 function App() {
   useEffect(() => {
     window.process = {
@@ -18,7 +18,6 @@ function App() {
       <Route exact path="/home" component={Home}/>
       <Route exact path="/details/:id" component={DogDetails}/>
       <Route exact path="/create" component={CreateDog}/>
-      <Route exact path="/home/config" component={Config}/>
     </div>
   );
 }
